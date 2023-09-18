642db6bb2b6bae4d1358e091693e5ae7cb060a9c
Gayathri <gayathri@wearetelescopic.co.uk>
2023-09-18 12:29:45 +0100

    components added

--- src/main.ts
+++ src/main.ts
@@ -4,27 +4,27 @@ import { createCustomElement } from '@angular/elements';
 import { InputComponent } from './app/ui/input/input.component';
 import { ButtonComponent } from './app/ui/button/button.component';
 
-import { AppModule } from './app/app.module';
+//import { AppModule } from './app/app.module';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+//platformBrowserDynamic().bootstrapModule(AppModule)
+  //.catch(err => console.error(err));
 
-  // (async () => {
-  //   const app = await createApplication({
-  //     providers:[]
-  //   });
+  (async () => {
+    const app = await createApplication({
+      providers:[]
+    });
 
-  //   const inputElement = createCustomElement(ButtonComponent,{
-  //     injector : app.injector,
-  //   });
+    const inputElement = createCustomElement(ButtonComponent,{
+      injector : app.injector,
+    });
     
-  //   customElements.define('dbs-click', inputElement)
+    customElements.define('dbs-click', inputElement)
 
-  //   const ButtonElement= createCustomElement(InputComponent,{
-  //     injector : app.injector,
-  //   });
+    const ButtonElement= createCustomElement(InputComponent,{
+      injector : app.injector,
+    });
     
-  //   customElements.define('dbs-text', ButtonElement)
+    customElements.define('dbs-text', ButtonElement)
 
-  // })()
+  })()
